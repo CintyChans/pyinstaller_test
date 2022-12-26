e244bf8d5123543a857f8df8faa57abcfd508110
Cinty Chan <100273954+CintyChans@users.noreply.github.com>
2022-12-26 09:42:06 +0800

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,2 +1,18 @@
 from main import *
-
+import argparse
+def test_1():
+  f1()
+  f2()
+  f5()
+  f4()
+  f6()
+  f7()
+  
+def test_2():
+  parser = argparse.ArgumentParser(description='Demo of argparse')
+  parser.add_argument('--line', type=int, default=1)
+  args = parser.parse_args()
+  if args.line:
+     print('argparse:',args.line)
+  print(args)
+  return args
