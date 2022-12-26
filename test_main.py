f64978ea2c384ae4ba3e033963050f8bd41426ab
Cinty Chan <100273954+CintyChans@users.noreply.github.com>
2022-12-26 14:44:12 +0800

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,20 +3,18 @@ import main
 import numpy as np
 class TestDemo(unittest.TestCase):
      def setUp(self):
-        print('setUp')
- 
-    # 用例的后置清理
-    def tearDown(self):
-        print('tearDown')
-    def test_demo_1(self):
-      person=main.f2()
-      self.assertEqual(person.id,1)
+          print('setUp')
+     def tearDown(self):
+          print('tearDown')
+     def test_demo_1(self):
+          person=main.f2()
+          self.assertEqual(person.id,1)
       
-    def test_demo_2(self):
-      self.assertEqual(main.f5(),np.array([1,2,3]))
+     def test_demo_2(self):
+          self.assertEqual(main.f5(),np.array([1,2,3]))
   
-    def test_demo_3(self):
-      self.assertTrue(main.f6()!=3.14)
+     def test_demo_3(self):
+          self.assertTrue(main.f6()!=3.14)
 
 if __name__ == '__main__':
-    unittest.main()
+     unittest.main()
