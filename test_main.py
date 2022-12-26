1a13a44acb5b92c49ee7fc46ba3016f9ab8f13cd
Cinty Chan <100273954+CintyChans@users.noreply.github.com>
2022-12-26 09:49:38 +0800

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,16 +3,11 @@ import argparse
 def test_1():
   f1()
   f2()
+  f3()
   f5()
   f4()
   f6()
   f7()
   
-def test_2():
-  parser = argparse.ArgumentParser(description='Demo of argparse')
-  parser.add_argument('--line', type=int, default=1)
-  args = parser.parse_args()
-  if args.line:
-     print('argparse:',args.line)
-  print(args)
-  return args
+  
+
